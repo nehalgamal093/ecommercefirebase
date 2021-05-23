94fc108f1b971ab8a9c6a33531a0b9644febebd8
Nehal <nehalgamal093@gmail.com>
2021-05-23 21:26:42 +0200

    Firestore firebase

--- lib/screens/addProduct.dart
+++ lib/screens/addProduct.dart
@@ -2,7 +2,7 @@ import 'package:ecommercefirebase/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
 class AddProduct extends StatelessWidget {
-  String _name, _price, _description, _category;
+  String _name, _price, _description, _category,_imageLocation;
   static String id = 'AddProduct';
   @override
   Widget build(BuildContext context) {
